54aa1faf80bbf6bb361be0bc8064e20f4a7c70fd
Kaiyi <alice7740@gmail.com>
2015-08-17 23:18:12 +0800

    no message

--- blescan.py
+++ blescan.py
@@ -224,6 +224,7 @@ def parse_events(sock, loop_count=100):
 				
 				for i in range(0, num_reports):
 
+					print pkt[9]
 					if pkt[9] == 1e:
 						Adstring = extract_beacon_data(pkt)
 					else:
