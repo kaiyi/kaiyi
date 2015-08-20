ced3bb0b01d9fc41efdb0b09d9f5851626e42b43
Kaiyi <alice7740@gmail.com>
2015-08-20 19:15:26 +0800

    Make as class

--- Ble.py
+++ Ble.py
@@ -4,89 +4,57 @@ import Queue
 import Blecontrl
 
 #user param
-PKT_QUEUE = Queue.Queue()
-l_device = []
-l_beacon = []
 
+class Ble:
 
+	def __init__(self):
+		self.l_device = []
+		self.l_beacon = []
+		self.sock = Blecontrl.init_ble()
 
-def BleConfig():
-	print "Configuring..."
-	os.popen("hciconfig hci0 down")
-	os.popen("hciconfig hci0 up")
-	
-	sock = Blecontrl.init_ble()
-	print "Config Finish"
-	return sock
-
-def BleScan(sock):
-
-	print "Scanning..."
-	
-	PKT_QUEUE = Blecontrl.ble_scan(sock)
-	
-	
-	while not PKT_QUEUE.empty():
-		Str = Blecontrl.parse_events(PKT_QUEUE)
-		#print len(Str)
-		if len(Str) == 7 :
-			l_device.append(Str)
-		elif len(Str) == 6:
-			l_beacon.append(Str)
-			
-	print "Scan Finish."
-	#print "Result: "
-	#print 'Devices',
-	#print '\n\t'.join([repr(x) for x in l_device])
-	#print 'Beacons',
-	#print '\n\t'.join([repr(x) for x in l_beacon])
-	return len(l_beacon)+len(l_device)
-	
-def BleAdvertise():
-	print "Advertising..."
-	Blecontrl.ble_adv()
-	print "Advertise Finish"
-
-def getDevicePktList():
-	return l_device
-
-def getBeaconPktList():	
-	return l_beacon
-
-def resetPktList():
-	del l_device[:]
-	del l_beacon[:]
-	
-def main():
-
-	sock = BleConfig()
-	
-	while 1:
-		n = BleScan(sock)
+	def BleConfig(self):
+		print "Configuring..."
+		os.popen("hciconfig hci0 down")
+		os.popen("hciconfig hci0 up")
 		
-		if n == 0:
-			print "No data!"
-		else:	
-			dev = getDevicePktList();
-			bea = getBeaconPktList();
-			print 'Devices',
-			print '\n\t'.join([repr(x) for x in dev])
-			print 'Beacons',
-			print '\n\t'.join([repr(x) for x in bea])
-			
-		resetPktList()
+		print "Config Finish"
+
+	def BleScan(self):
+
+		print "Scanning..."
 		
-		#print len(l_device)
-		#print len(l_beacon)
-			
+		PKT_QUEUE = Blecontrl.ble_scan(self.sock)
 		
-		BleAdvertise()
-
-		print "done"
-
-
-
-if __name__ == "__main__":
-	main()
 		
+		while not PKT_QUEUE.empty():
+			Str = Blecontrl.parse_events(PKT_QUEUE)
+			#print len(Str)
+			if len(Str) == 7 :
+				self.l_device.append(Str)
+			elif len(Str) == 6:
+				self.l_beacon.append(Str)
+				
+		print "Scan Finish."
+		#print "Result: "
+		#print 'Devices',
+		#print '\n\t'.join([repr(x) for x in l_device])
+		#print 'Beacons',
+		#print '\n\t'.join([repr(x) for x in l_beacon])
+		return len(self.l_beacon)+len(self.l_device)
+	
+	def BleAdvertise(self):
+		print "Advertising..."
+		Blecontrl.ble_adv()
+		print "Advertise Finish"
+
+	def getDevicePktList(self):
+		return self.l_device
+
+	def getBeaconPktList(self):	
+		return self.l_beacon
+
+	def resetPktList(self):
+		del self.l_device[:]
+		del self.l_beacon[:]
+
 
---
+++ test.py
@@ -0,0 +1,31 @@
+import Ble
+	
+def main():
+
+	mBle = Ble()
+	
+	while 1:
+		n = mBle.BleScan(sock)
+		
+		if n == 0:
+			print "No data!"
+		else:	
+			dev = mBle.getDevicePktList();
+			bea = mBle.getBeaconPktList();
+			print 'Devices',
+			print '\n\t'.join([repr(x) for x in dev])
+			print 'Beacons',
+			print '\n\t'.join([repr(x) for x in bea])
+			
+		mBle.resetPktList()
+		
+		#print len(l_device)
+		#print len(l_beacon)
+			
+		
+		mBle.BleAdvertise()
+
+		print "done"
+		
+if __name__ == "__main__":
+	main()
