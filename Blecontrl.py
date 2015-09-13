20f7585bc3f9856c01232ca910fb8e89597a098a
Kaiyi <alice7740@gmail.com>
2015-09-13 13:09:49 +0800

    no message

--- Blecontrl.py
+++ Blecontrl.py
@@ -291,9 +291,9 @@ def ble_adv(packet,name,cx,cy,stdev,floor,tvalue):
 	cyHexStr = float_to_hex(cy)
 	stdevHexStr = float_to_hex(stdev)
 	floorHexStr = hex(floor)
-	tValueHexStr = hex(tValue)
+	tvalueHexStr = hex(tvalue)
 	
-	print "name: "+nameHexStr+" cx: "+cxHexStr+" cy: "+cyHexStr+" std: "+stdevHexStr+" floor: "+floorHexStr+" tValue: "+tValueHexStr
+	print "name: "+nameHexStr+" cx: "+cxHexStr+" cy: "+cyHexStr+" std: "+stdevHexStr+" floor: "+floorHexStr+" tValue: "+tvalueHexStr
 	
 	subprocess.Popen("hcitool -i hci0 cmd 0x08 0x0008 1e 02 01 1a 1a ff 4c 00 02 15 e2 c5 6d b5 df fb 48 d2 b0 60 d0 f5 a7 10 96 e0 00 00 00 00 c5 00 00 00 00 00 00 00 00 00 00 00 00 00",shell=True)
 	proc = subprocess.Popen(["hciconfig", "hci0", "leadv", "0"])
